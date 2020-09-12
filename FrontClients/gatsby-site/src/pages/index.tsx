c283d319be03b56b5bc639fda8ac80949271f663
纷纷飞花 <943866961@qq.com>
2020-09-12 11:29:06 +0800

    update

--- FrontClients/gatsby-site/src/pages/index.tsx
+++ FrontClients/gatsby-site/src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from "gatsby"
 import React, { useState } from "react"
 
 export default () => {
 
   return <>
     <div>
-
+      <Link to={"/userPages/"}>用户管理</Link>
     </div>
   </>
 }
